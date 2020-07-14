96dc4310c694f0eb2d86b72e5fc3189988e25521
鲱鱼 <lf18234129242@163.com>
2020-07-14 22:51:11 +0800

    chart制作

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>
+  <div ref="chartDom">
 
   </div>
 </template>
 
 <script>
-  export default {
-    
+import debounce from 'lodash/debounce'
+import echarts from 'echarts'
+import { addListener, removeListener } from 'resize-detector';
+export default {
+  data() {
+    return {
+      chart: null
+    }
+  },
+  props: {
+    options: {
+      type: Object,
+      default: () => {}
+    },
+  },
+  watch: {
+    options: {
+      deep: true,
+      handler(n) {
+        this.chart.setOption(n)
+      }
+    }
+  },
+  created() {
+    this.resize = debounce(this.resize, 300)
+  },
+  mounted () {
+    this.renderChart()
+    addListener(this.$refs.chartDom, this.resize)
+  },
+  beforeDestroy() {
+    removeListener(this.$refs.chartDom, this.resize)
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    resize() {
+      this.chart.resize()
+    },
+    renderChart() {
+      this.chart = echarts.init(this.$refs.chartDom)
+      this.chart.setOption(this.options)
+    }
   }
+}
 </script>
 
-<style lang="scss" scoped>
+<style lang="less" scoped>
 
 </style>
--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,13 +1,46 @@
 <template>
   <div>
-    Analysis分析页
+    <Chart :options="chartOption" style="height:400px"></Chart>
   </div>
 </template>
 
 <script>
+import Chart from '@/components/Chart'
+import random from 'lodash/random'
 export default {
-  name: 'analysis'
-  
+  name: 'analysis',
+  components: {
+    Chart
+  },
+  data() {
+    return {
+      chartOption: {
+        title: {
+          text: '入门示例'
+        },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '鞋子', '褥子', '袜子', '高跟鞋']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5,10,20,40,30,60]
+        }]
+      }
+    }
+  },
+  mounted () {
+    this.interval = setInterval(() => {
+      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+        () => random(100)
+      )
+    }, 3000);
+  },
+  beforeDestroy() {
+    clearInterval(this.interval)
+  }
 }
 </script>
 
