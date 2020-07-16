ff35f6f46be5789968b651a7fdfdfcbcfecd6be8
鲱鱼 <lf18234129242@163.com>
2020-07-16 21:14:04 +0800

    添加注释

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,12 +1,22 @@
+/**
+ * 获取并返回当前权限
+ */
 export function getCurrentAuthority() {
   return ['admin']
 }
 
+/**
+ * 检测当前权限
+ * @param { Array } authority 
+ */
 export function check(authority) {
   const current = getCurrentAuthority()
   return current.some(item => authority.includes(item))
 }
 
+/**
+ * 检测登录状态
+ */
 export function isLogin() {
   const current = getCurrentAuthority()
   return current && current[0] !== 'guest'
