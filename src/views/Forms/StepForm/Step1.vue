ec08f23c2cb8614d3228302cacb70b64fcc4ed15
鲱鱼 <lf18234129242@163.com>
2020-07-22 09:19:35 +0800

    表单验证

--- src/views/Forms/StepForm/Step1.vue
+++ src/views/Forms/StepForm/Step1.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    第一步
+    <a-form layout="horizontal" :form="form">
+      <a-form-item
+        label="付款账户"
+        :labelCol="formItemLayout.labelCol"
+        :wrapperCol="formItemLayout.wrapperCol"
+      >
+        <a-input></a-input>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style lang="less" scoped>
-
 </style>
